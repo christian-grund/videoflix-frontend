477bb0e2fe02ffd94ccf31bbedeb3437743610ce
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-08-30 12:08:46 +0200

    Header styling

--- videoflix/src/app/app.component.ts
+++ videoflix/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { StartpageComponent } from './startpage/startpage.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, StartpageComponent],
+  imports: [CommonModule, RouterOutlet, StartpageComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'videoflix';
