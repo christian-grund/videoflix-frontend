0351838eac82f7659ec3f1c3b511602ce27d7a05
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-08-30 13:30:39 +0200

    Footer styling

--- videoflix/src/app/startpage/startpage.component.ts
+++ videoflix/src/app/startpage/startpage.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/components/header/header.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-startpage',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './startpage.component.html',
-  styleUrl: './startpage.component.scss'
+  styleUrl: './startpage.component.scss',
 })
-export class StartpageComponent {
-
-}
+export class StartpageComponent {}
