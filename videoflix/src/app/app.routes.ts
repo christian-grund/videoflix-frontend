08c50ceb36663d2e910e44fd2a6a9e98e37858b0
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-09-03 13:14:30 +0200

    Header buttons depending on routerlink

--- videoflix/src/app/app.routes.ts
+++ videoflix/src/app/app.routes.ts
@@ -6,9 +6,11 @@ import { ImprintComponent } from './textsites/imprint/imprint.component';
 import { DataprivacyComponent } from './textsites/dataprivacy/dataprivacy.component';
 import { ResetpasswordComponent } from './auth/resetpassword/resetpassword.component';
 import { ForgotpasswordComponent } from './auth/forgotpassword/forgotpassword.component';
+import { VideoofferComponent } from './videooffer/videooffer.component';
 
 export const routes: Routes = [
   { path: '', component: StartpageComponent },
+  { path: 'videos', component: VideoofferComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'forgotpassword', component: ForgotpasswordComponent },
--- videoflix/src/app/shared/components/header/header.component.ts
+++ videoflix/src/app/shared/components/header/header.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  currentUrl!: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    this.route.url.subscribe((segments) => {
+      this.currentUrl = '/' + segments.map((segment) => segment.path).join('/');
+      this.changeDetectorRef.detectChanges(); // Hier wird die Change Detection ausgelöst
+      console.log('currentUrl:', this.currentUrl);
+    });
+  }
+}
