5c58f61f565b268163f36ae94ed9001dd0da0120
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-09-05 11:10:28 +0200

    Login logic & styling

--- videoflix-frontend/src/app/auth/login/login.component.ts
+++ videoflix-frontend/src/app/auth/login/login.component.ts
@@ -2,12 +2,29 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterModule],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    RouterModule,
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  showPassword: boolean = false;
+  matchError: boolean = false;
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+}
