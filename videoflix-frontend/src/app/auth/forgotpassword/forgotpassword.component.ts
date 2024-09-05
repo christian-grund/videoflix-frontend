7f6c05d4bd1c57ee8f668a34b24de543021d6333
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-09-05 13:41:46 +0200

    ResetPassword styling & matchError logic

--- videoflix-frontend/src/app/auth/forgotpassword/forgotpassword.component.ts
+++ videoflix-frontend/src/app/auth/forgotpassword/forgotpassword.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-forgotpassword',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, FormsModule, CommonModule],
   templateUrl: './forgotpassword.component.html',
   styleUrl: './forgotpassword.component.scss',
 })
-export class ForgotpasswordComponent {}
+export class ForgotpasswordComponent {
+  email: string = '';
+  emailValid: boolean = false;
+  emailError: boolean = false;
+
+  onSubmit() {
+    this.emailError = !this.emailValid;
+  }
+
+  onEmailChange(value: string) {
+    this.email = value;
+    this.emailValid = this.validateEmail(value);
+  }
+
+  validateEmail(email: string): boolean {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  }
+}
--- videoflix-frontend/src/app/auth/resetpassword/resetpassword.component.ts
+++ videoflix-frontend/src/app/auth/resetpassword/resetpassword.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resetpassword',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, FormsModule, CommonModule],
   templateUrl: './resetpassword.component.html',
   styleUrl: './resetpassword.component.scss',
 })
-export class ResetpasswordComponent {}
+export class ResetpasswordComponent {
+  password: string = '';
+  confirmPassword: string = '';
+  matchError: boolean = false;
+
+  onSubmit() {
+    this.matchError = !this.passwordMatch(); // Setzt den matchError direkt basierend auf dem Vergleich
+    console.log('passwordMatch:', !this.matchError); // Zeigt das Gegenteil von matchError an
+  }
+
+  passwordMatch(): boolean {
+    return this.password === this.confirmPassword; // Direkter Vergleich mit Rückgabewert
+  }
+}
