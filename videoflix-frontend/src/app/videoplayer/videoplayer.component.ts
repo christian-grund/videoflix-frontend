b77d67cafa6ee407c6c1cd19243efc3a49fbeb84
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-09-10 13:37:15 +0200

    Video fullscreen custom controls

--- videoflix-frontend/src/app/videoplayer/videoplayer.component.ts
+++ videoflix-frontend/src/app/videoplayer/videoplayer.component.ts
@@ -27,11 +27,14 @@ export class VideoplayerComponent implements OnInit, OnDestroy, AfterViewInit {
   isMuted = false;
   isFullscreen = false;
   isResolutionMenuVisible = false;
+  isVolumeSliderVisible = false;
   videoDuration: number = 0;
   currentTime: number = 0;
   progress: number = 0;
   hoverProgress = 0;
   hovering = false;
+
+  currentVolume: number = 0.5;
   currentResolution: number = 720;
   videoBasePath = '../../assets/video/';
   iconBasePath = '../../assets/img/icons/videoplayer/';
@@ -79,6 +82,11 @@ export class VideoplayerComponent implements OnInit, OnDestroy, AfterViewInit {
       videoPlayer.addEventListener('timeupdate', () => {
         this.currentTime = videoPlayer.currentTime;
         this.cdr.detectChanges();
+      });
+
+      videoPlayer.addEventListener('fullscreenchange', () => {
+        this.isFullscreen = !!document.fullscreenElement;
+        this.updateControlsVisibility();
       });
     }
   }
@@ -155,18 +163,48 @@ export class VideoplayerComponent implements OnInit, OnDestroy, AfterViewInit {
     }
   }
 
-  toggleMute() {
+  toggleVolumeSlider() {
+    this.isVolumeSliderVisible = !this.isVolumeSliderVisible;
+  }
+
+  setVolume(event: Event) {
     const videoPlayer = this.videoPlayer.nativeElement;
-    videoPlayer.muted = !videoPlayer.muted;
-    this.isMuted = videoPlayer.muted;
+    const inputElement = event.target as HTMLInputElement;
+
+    videoPlayer.volume = parseFloat(inputElement.value);
+    this.currentVolume = videoPlayer.volume;
+
+    if (this.currentVolume == 0) {
+      this.isMuted = true;
+    } else {
+      this.isMuted = false;
+    }
   }
 
   toggleFullscreen() {
-    const videoPlayer = this.videoPlayer.nativeElement;
+    const videoContainer = document.querySelector(
+      '.video-container'
+    ) as HTMLElement;
+
     if (document.fullscreenElement) {
       document.exitFullscreen();
     } else {
-      videoPlayer.requestFullscreen();
+      videoContainer
+        .requestFullscreen()
+        .then(() => {
+          // Optional: Update the controls visibility or any other logic after entering fullscreen
+          this.updateControlsVisibility();
+        })
+        .catch((err) => {
+          console.error('Failed to enter fullscreen mode:', err);
+        });
+    }
+  }
+
+  updateControlsVisibility() {
+    const controls = document.querySelector('.controls') as HTMLElement;
+    if (controls) {
+      controls.style.display = 'flex'; // Ensure the custom controls are always visible
     }
   }
 
